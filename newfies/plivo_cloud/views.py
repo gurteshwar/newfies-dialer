97a4c4511c39e8c67cfc7b51c968a36cf2a5742b
Gurteshwar <gurteshwar@plivo.com>
2013-07-17 16:06:17 +0000

    add OutboundCall class to plivo/vall_handler.py

--- newfies/plivo_cloud/views.py
+++ newfies/plivo_cloud/views.py
@@ -1,8 +1,18 @@
 # Create your views here.
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
+from plivo_cloud.call_handler import OutboundCall as out_handler
 
 @csrf_exempt
 def answer_url_handler(request):
-    print request
-    return HttpResponse()
+    call_direction = request.POST['Direction']
+    request_uuid = request.POST['RequestUUID']
+    print 'Direction -> %s RequestUUID -> %s' % (call_direction,request_uuid)
+    if call_direction == 'outbound':
+        handler = out_handler(request_uuid)
+        if handler:
+            handler.load_data()
+            xml_response = handler.generate_xml()
+    response = HttpResponse(xml_response)
+    response['Content-Type'] = 'application/xml'
+    return response
